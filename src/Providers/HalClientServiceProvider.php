87e7bf445141a367b1ae46dbc92c2d2363db70e3
Atta Khalid <amanank@yahoo.com>
2024-09-12 18:19:49 +0100

    Fixed config path in HalClientServiceProvider

--- src/Providers/HalClientServiceProvider.php
+++ src/Providers/HalClientServiceProvider.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Amanank\HalClient\Providers;
 
 use Illuminate\Support\ServiceProvider;
@@ -7,18 +6,38 @@ use Amanank\HalClient\Client;
 use Amanank\HalClient\Console\Commands\GenerateHalModels;
 
 class HalClientServiceProvider extends ServiceProvider {
+
+    /**
+     * The path to the configuration file.
+     */
+    private const CONFIG_PATH = __DIR__ . '/../config/hal-client.php';
+
+    /**
+     * The configuration key.
+     */
+    private const CONFIG_KEY = 'hal-client';
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register() {
+        $this->mergeConfigFrom(self::CONFIG_PATH, self::CONFIG_KEY);
+
         $this->app->singleton(Client::class, function ($app) {
-            $config = require __DIR__ . '/../../config/hal-client.php';
-            return new Client($config);
+            return new Client($app['config']->get(self::CONFIG_KEY));
         });
     }
 
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot() {
         // Optional: Publish the configuration file
-        $this->publishes([
-            __DIR__ . '/../../config/hal-client.php' => config_path('hal-client.php'),
-        ]);
+        $this->publishes([self::CONFIG_PATH => config_path(self::CONFIG_KEY . '.php')]);
 
         if ($this->app->runningInConsole()) {
             $this->commands([
